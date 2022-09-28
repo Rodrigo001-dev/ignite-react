5ed50c6a32f3a3036073e45b1d1144c01b1de90b
Rodrigo <rodrigorael53@gmail.com>
2022-09-28 07:48:26 -0300

    converting a file using bable/cli

--- reactjs/aulas/01-github-explorer/dist/bundle.js
+++ reactjs/aulas/01-github-explorer/dist/bundle.js
@@ -1,8 +1,11 @@
 "use strict";
 
-var _user$address;
+var _react = _interopRequireDefault(require("react"));
 
-var user = {
-  name: 'Rodrigo'
-};
-console.log((_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
+
+;
--- reactjs/aulas/01-github-explorer/src/index.js
+++ reactjs/aulas/01-github-explorer/src/index.js
@@ -1,5 +1,5 @@
-const user = {
-  name: 'Rodrigo',
-};
+import React from 'react';
 
-console.log(user.address?.street);
+function App() {
+  return <h1>Hello World</h1>
+};
