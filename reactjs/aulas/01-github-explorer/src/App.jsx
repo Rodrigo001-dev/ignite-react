1ca33e2eb43fb2a1c87283310f40ac082b584b4e
Rodrigo <rodrigorael53@gmail.com>
2022-09-28 19:50:31 -0300

    learn about immutability

--- reactjs/aulas/01-github-explorer/src/App.jsx
+++ reactjs/aulas/01-github-explorer/src/App.jsx
@@ -3,6 +3,12 @@ import { Counter } from './components/Counter';
 
 import './styles/global.scss';
 
+// imutabilidade
+// quando falamos de imutabilidade, nesse conceito queremos dizer que não é 
+// possível alterar diretamente uma informação de uma 
+// variável(que já está salva na memória) e sim criar um
+// novo espaço na memória contendo a minha nova informação
+
 export function App() {
 
   return (
--- reactjs/aulas/01-github-explorer/src/components/Counter.jsx
+++ reactjs/aulas/01-github-explorer/src/components/Counter.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 
+// imutabilidade
+// quando falamos de imutabilidade, nesse conceito queremos dizer que não é 
+// possível alterar diretamente uma informação de uma 
+// variável(que já está salva na memória) e sim criar um
+// novo espaço na memória contendo a minha nova informação
+
+// por isso que é colocado setCounter que é uma forma de alterar o valor de
+// counter, como o nome já diz eu posso setar(set) um novo valor para counter
+// não adicionar(addCounter), não incrementar(incrementCounter)
+// é como se eu tivesse criando uma nova variável counter toda vez que eu executar
+// o setCounter
+
 export function Counter() {
   // const => constant
   // let => let it change(deixe isso mudar)
