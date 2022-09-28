16151576bf84be1fa174feb969403df23e5e3e47
Rodrigo <rodrigorael53@gmail.com>
2022-09-28 18:28:09 -0300

    doing some validations to check which environment i am in

--- reactjs/aulas/01-github-explorer/webpack.config.js
+++ reactjs/aulas/01-github-explorer/webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // o webpack vai estipular uma série de configurações que é chamado de loaders
 // que vão ensinar para a nossa aplicação como deve tratar cada um dos tipos
 // de arquivos importados, básicamente o webpack vai pegar todos esses arquivos
 // e vai converter em arquivos que são entendíveis pelo browser diretemente
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   // o entry é para falar qual é o arquivo inicial da aplicação
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   // o output é para falar qual arquivo que eu vou gerar com o webpack
@@ -17,6 +21,16 @@ module.exports = {
     // estou falando qual o tipo de arquivo ele vai ler
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    // no static eu tenho que falar onde está o arquivo com o conteúdo
+    // estático da aplicação
+    static: path.resolve(__dirname, 'public'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   // no module vão ficar as configurações de como a aplicação vai se comportar
   // quando eu estiver importando cada um dos tipos de arquivos
   module: {
