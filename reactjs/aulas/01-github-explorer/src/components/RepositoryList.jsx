84e62302976058d5f33ea2ec8a004495290bcaa6
Rodrigo <rodrigorael53@gmail.com>
2022-09-28 23:35:34 -0300

    getting the repos from github API and rendering on screen with useEffect

--- reactjs/aulas/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/aulas/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,17 +1,26 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository="unform2" />
-        <RepositoryItem repository="unform2" />
-        <RepositoryItem repository="unform2" />
-        <RepositoryItem repository="unform2" />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   );
