ce5c14419003895d60e48fee843431e4a2da097e
Rodrigo <rodrigorael53@gmail.com>
2022-09-29 10:11:02 -0300

    finishin the Sidebar component

--- reactjs/desafios/ignite-challenge-02/src/components/SideBar.tsx
+++ reactjs/desafios/ignite-challenge-02/src/components/SideBar.tsx
@@ -1,4 +1,29 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+
+import { api } from "../services/api";
+
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+};
+
+export function SideBar({ handleClickButton, selectedGenreId }: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
   return (
     <nav className="sidebar">
       <span>Watch<p>Me</p></span>
