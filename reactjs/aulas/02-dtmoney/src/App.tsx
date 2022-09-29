a0545870f114080146b7ff6ef608b2b007f9fb03
Rodrigo <rodrigorael53@gmail.com>
2022-09-29 18:32:39 -0300

    styling with the styled-componets

--- reactjs/aulas/02-dtmoney/src/App.tsx
+++ reactjs/aulas/02-dtmoney/src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #8257E6;
+`;
 
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello World</h1>
+      <Title className="title">Hello World</Title>
     </div>
   );
 }
