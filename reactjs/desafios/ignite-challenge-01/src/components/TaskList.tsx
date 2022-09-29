e37d691be108f2141996c39a9893946e9bbcde39
Rodrigo <rodrigorael53@gmail.com>
2022-09-29 07:48:34 -0300

    finishin the function handleRemoveTask

--- reactjs/desafios/ignite-challenge-01/src/components/TaskList.tsx
+++ reactjs/desafios/ignite-challenge-01/src/components/TaskList.tsx
@@ -34,6 +34,10 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
