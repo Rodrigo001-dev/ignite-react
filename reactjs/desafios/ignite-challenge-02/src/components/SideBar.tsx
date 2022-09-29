caa5b5e1752ca8422ace89185975e801a7d341f9
Rodrigo <rodrigorael53@gmail.com>
2022-09-29 09:55:47 -0300

    finishin the Content component

--- reactjs/desafios/ignite-challenge-02/src/components/SideBar.tsx
+++ reactjs/desafios/ignite-challenge-02/src/components/SideBar.tsx
@@ -1,3 +1,20 @@
 export function SideBar() {
-  // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
