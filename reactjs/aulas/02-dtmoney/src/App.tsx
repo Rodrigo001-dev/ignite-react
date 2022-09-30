def622ef45921f53521dfc537a9d4097534f2b08
Rodrigo <rodrigorael53@gmail.com>
2022-09-30 03:16:15 -0300

    separating the Modal into a component and applying some styles to the overlay and content of the modal

--- reactjs/aulas/02-dtmoney/src/App.tsx
+++ reactjs/aulas/02-dtmoney/src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
 
@@ -26,12 +27,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
--- reactjs/aulas/02-dtmoney/src/styles/global.ts
+++ reactjs/aulas/02-dtmoney/src/styles/global.ts
@@ -70,4 +70,20 @@ export const GlobalStyle = createGlobalStyle`
     opacity: 0.6;
     cursor: not-allowed;
   }
+
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex ;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {}
 `;
