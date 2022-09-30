48d93916d02fbb9954ae68e22eff4dba6931bb55
Rodrigo <rodrigorael53@gmail.com>
2022-09-30 08:05:51 -0300

    cusing the color of selected buttons to change dynamically

--- reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-import { Container, TrasactionTypeContainer } from './styles';
+import { Container, TrasactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -15,6 +16,8 @@ export function NewTransactionModal({
   isOpen, 
   onRequestClose 
 }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal 
       isOpen={isOpen} 
@@ -38,19 +41,25 @@ export function NewTransactionModal({
         <input type="number" placeholder="Valor" />
 
         <TrasactionTypeContainer>
-          <button
+          <RadioBox
             type="button"
+            onClick={() => { setType('deposit') }}
+            isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button
+          <RadioBox
             type="button"
+            onClick={() => { setType('withdraw') }}
+            isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TrasactionTypeContainer>
 
         <input placeholder="Categoria" />
--- reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/styles.ts
+++ reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,4 @@
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.form`
@@ -56,38 +56,52 @@ export const TrasactionTypeContainer = styled.div`
   display: grid;
   grid-template-columns:  1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #D7D7D7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+};
 
-    background: transparent;
+const colors = {
+  green: '#33CC95',
+  red: '#E52E4D'
+};
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #D7D7D7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
+  background: ${(props) => props.isActive 
+  // transparentize vai deixar a cor mais transparente
+  ? transparentize(0.9, colors[props.activeColor])
+  : 'transparent'
+};
 
-    &:hover {
-      /* 
-        o darken vai escurecer uma cor, nesse caso vai escurecer a cor #D7D7D7
-        em 10%(0.1)
-      */
-      border-color: ${darken(0.1, '#D7D7D7')};
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  transition: border-color 0.2s;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  &:hover {
+    /* 
+      o darken vai escurecer uma cor, nesse caso vai escurecer a cor #D7D7D7
+      em 10%(0.1)
+    */
+    border-color: ${darken(0.1, '#D7D7D7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
