819d9bae6d9f11a04c954d65abe926badc21862b
Rodrigo <rodrigorael53@gmail.com>
2022-09-30 08:23:14 -0300

    getting the data inside input through states

--- reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/aulas/02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
@@ -16,7 +16,16 @@ export function NewTransactionModal({
   isOpen, 
   onRequestClose 
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    // por padrão quando o form realiza o submit ele recarrega a página
+    // essa linha é para previnir esse comportamento padrão
+    event.preventDefault();
+  };
 
   return (
     <Modal 
@@ -33,12 +42,20 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder="Título" />
+        <input 
+          placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
 
-        <input type="number" placeholder="Valor" />
+        <input type="number" 
+          placeholder="Valor" 
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
 
         <TrasactionTypeContainer>
           <RadioBox
@@ -62,7 +79,11 @@ export function NewTransactionModal({
           </RadioBox>
         </TrasactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input 
+          placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
 
         <button type="submit">
           Cadastrar
