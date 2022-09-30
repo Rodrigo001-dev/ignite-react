bb0275c5875d39bacaa9ec29ee86e1ccc50ad8e3
Rodrigo <rodrigorael53@gmail.com>
2022-09-30 08:42:13 -0300

    creating datas to be displayed on screen

--- reactjs/aulas/02-dtmoney/src/index.tsx
+++ reactjs/aulas/02-dtmoney/src/index.tsx
@@ -11,6 +11,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Cassa',
+          amount: 1100,
+          createdAt: new Date('2022-02-14 11:00:00')
+        }
+      ],
+    })
+  },
+
   routes() {
     // estou dizendo para o miragejs que todas as chamadas a api que eu vou fazer
     // vão estar a partir do endereço http://localhost:3000/api
