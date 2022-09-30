a02b58b8b4795d75ba7b99a1a9d25c3c5d4c33e3
Rodrigo <rodrigorael53@gmail.com>
2022-09-30 00:30:42 -0300

    installing and using the miragejs to create a fake API

--- reactjs/aulas/02-dtmoney/src/index.tsx
+++ reactjs/aulas/02-dtmoney/src/index.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
+
 import { App } from './App';
+
+// o miragejs server para criar uma fake API
+createServer({
+  routes() {
+    // estou dizendo para o miragejs que todas as chamadas a api que eu vou fazer
+    // vão estar a partir do endereço http://localhost:3000/api
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
