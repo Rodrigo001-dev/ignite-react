5352f0cdcfb5a92e497a38f0879e4f406eca226a
Rodrigo <rodrigorael53@gmail.com>
2022-10-01 04:06:01 -0300

    finishin the addProduct function

--- reactjs/desafios/ignite-challenge-03/src/hooks/useCart.tsx
+++ reactjs/desafios/ignite-challenge-03/src/hooks/useCart.tsx
@@ -23,28 +23,75 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    // buscar os dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    // se não for null vai entrar dentro do if e rotornar o valor transformando
+    // ele no valor originial JSON.parse(storagedCart)
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
+    // o storagedCart poder ser null, se ela for null eu vou retornar um [] vazio
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      // dessa forma o updatedCart é um novo [] com os valores de cart
+      // qualquer alteração que eu fizer no updatedCart não vai refletir no cart
+      // para respeitar a regra da imutabilidade
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      // se o produto existe no carrinho eu vou pegar o amount dele
+      // se não existe no carrinho a quantidade dele é zero
+      const currentAmount = productExists ? productExists.amount : 0;
+      // quantidade desejada
+      const amount = currentAmount + 1;
+
+      // se a quantidade desejada(amount) for maior que a quantidade que eu tenho
+      // em estoque vai dar um erro
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      // se o produto existe vai ser atualizado a quantidade de produto
+      if (productExists) {
+        productExists.amount = amount;
+      } else {
+        // se for um produto novo(que não foi adicionado no carrinho)
+        const product = await api.get(`/produts/${productId}`);
+
+        // para acidionar um novo produto no carrinho eu preciso pegar os dados
+        // que foram retornados da api e adiconar o campo amount como 1
+        // porque foi a primeira vez que ele foi acicionado
+        const newProduct = {
+          ...product.data,
+          amount: 1
+        };
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
