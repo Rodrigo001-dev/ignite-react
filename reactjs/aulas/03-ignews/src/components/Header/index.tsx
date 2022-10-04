7fd7392ee28fc9d296960b62557b223e52861794
Rodrigo <rodrigorael53@gmail.com>
2022-10-04 13:25:00 -0300

    creating the SignInButton component

--- reactjs/aulas/03-ignews/src/components/Header/index.tsx
+++ reactjs/aulas/03-ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 // quando utilizamos Next todas as imagens ficam dentro da pasta public
 
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -11,6 +13,8 @@ export function Header() {
           <a className={styles.active} href="">Home</a>
           <a href="">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
