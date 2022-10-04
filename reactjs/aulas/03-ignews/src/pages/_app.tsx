d73d38924d8b542d20da1f466aaf2cf5029285fb
Rodrigo <rodrigorael53@gmail.com>
2022-10-04 10:31:37 -0300

    creting the _document to add the Roboto font

--- reactjs/aulas/03-ignews/src/pages/_app.tsx
+++ reactjs/aulas/03-ignews/src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import { AppProps } from 'next/app';
 
+// o _app.tsx é um componente sempre vai ficar por volta de todas as páginas, ou
+// seja, quando estmaos acessando uma página no Next.js na verdade o que estamos
+// acessando é esse componente(MyApp) e esse componente vai mostrar a página
+// quando o Next vai acessar a Home por exemplo, na verdadae ele acessa o _app e
+// mostra a home no lugar do Component
+// se eu quiser que uma coisa repita em todas as páginas, eu vou colocar aqui
+// dentro
+// toda vez que o usuário troca de tela, o _app é reexecutado, então tudo o que
+// tiver aqui dentro de chamada HTTP, um estado, tudo vai ser recriado do zero,
+// toda vez que o usuário trocar de tela
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
---
+++ reactjs/aulas/03-ignews/src/pages/_document.tsx
@@ -0,0 +1,38 @@
+// o _document funciona de forma semelhante ao _app mas ele é carregado apenas
+// uma unica vez na aplicação
+// o _document pode ser comparado com o index.html que tinha na pasta public em
+// aplicações React
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+          <title>ig.news</title>
+        </Head>
+        <body>
+          {
+            /* 
+              esse Main quer dizer que todo todo o conteudo da aplicação vai ser
+              renderizado no Main
+            */
+          }
+          <Main />
+          {
+            /*
+              o NextScript é onde o Next vai colocar os arquivos JS que a
+              aplicação precisa para funcionar
+            */
+          }
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+};
