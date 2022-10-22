d556e5042c6ed9f2c865b12cd7e108cea81f52b6
Rodrigo <rodrigorael53@gmail.com>
2022-10-22 12:19:42 -0300

    using the Link tag from Next to work with the SPA concept and using the prefetch property

--- reactjs/aulas/03-ignews/src/components/Header/index.tsx
+++ reactjs/aulas/03-ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 // quando utilizamos Next todas as imagens ficam dentro da pasta public
 
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
@@ -10,8 +12,26 @@ export function Header() {
       <div className={styles.hedaerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+          {
+            /*
+              utilizando a tag Link do Next fazemos o uso do conceito de SPA do
+              React(basicamente estou reaproveitando o core da aplicação, a 
+              estrutura da aplicação, mudando só o conteúdo)
+            */
+          }
+          <Link href="/">
+            <a className={styles.active} href="">Home</a>
+          </Link>
+
+          {
+            /* 
+              o prefetch vai fazer com que o Next por baixo dos panos deixe a
+              página de posts pré-carregada
+            */
+          }
+          <Link href="/posts" prefetch>
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
