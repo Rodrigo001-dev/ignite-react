b3065d2e52806627db316e6159441e80d01396ad
Rodrigo <rodrigorael53@gmail.com>
2022-10-25 17:52:24 -0300

    adding fonts from the google fonts

--- reactjs/desafios/ignite-challenge-05/src/components/Header/index.tsx
+++ reactjs/desafios/ignite-challenge-05/src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function Header() {
+  return (
+    <header>
+      <div>
+        <h1>ola</h1>
+      </div>
+    </header>
+  );
 }
