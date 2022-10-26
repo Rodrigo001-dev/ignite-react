5ad2ba669dabee74ff974aa6f0cc9de3f35d1208
Rodrigo <rodrigorael53@gmail.com>
2022-10-25 21:20:30 -0300

    assembling the html and styling the home page

--- reactjs/desafios/ignite-challenge-05/src/pages/index.tsx
+++ reactjs/desafios/ignite-challenge-05/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +26,84 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | SpaceTraveling</title>
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <a>
+            <strong>Titulo do post</strong>
+            <p>Subtitulo do post asdfeasdfeawfdsafeasdfeafdsfeadfaefasdfe</p>
+            <div>
+              <span>
+                <FiCalendar size="1.5rem" />
+                <time>15 Jan 2022</time>
+              </span>
 
-//   // TODO
-// };
+              <span>
+                <FiUser size="1.5rem" />
+                Author do post
+              </span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Titulo do post</strong>
+            <p>Subtitulo do post asdfeasdfeawfdsafeasdfeafdsfeadfaefasdfe</p>
+            <div>
+              <span>
+                <FiCalendar size="1.5rem" />
+                <time>15 Jan 2022</time>
+              </span>
+
+              <span>
+                <FiUser size="1.5rem" />
+                Author do post
+              </span>
+            </div>
+          </a>
+
+          <a>
+            <strong>Titulo do post</strong>
+            <p>Subtitulo do post asdfeasdfeawfdsafeasdfeafdsfeadfaefasdfe</p>
+            <div>
+              <span>
+                <FiCalendar size="1.5rem" />
+                <time>15 Jan 2022</time>
+              </span>
+
+              <span>
+                <FiUser size="1.5rem" />
+                Author do post
+              </span>
+            </div>
+          </a>
+        </div>
+
+        <div className={styles.containerButton}>
+          {/* {
+            postsPagination.next_page !== null
+            &&
+            <button>
+              Carregar mais posts
+            </button>
+          } */}
+          <button>Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  // const postsResponse = await prismic.getByType();
+
+  return {
+    props: {}
+  }
+};
