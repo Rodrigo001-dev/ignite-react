41752a8823a662f08d51d1af76130ccb89c2e99b
Rodrigo <rodrigorael53@gmail.com>
2022-11-07 17:03:23 -0300

    creating the home page

--- reactjs/aulas/04-dashgo/src/pages/index.tsx
+++ reactjs/aulas/04-dashgo/src/pages/index.tsx
@@ -1,5 +1,66 @@
+import { Button, Flex, Input, Stack, FormLabel, FormControl } from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        {
+          /*
+           o Stack serve para quando for criar uma pilha de elementos que precisam
+           de um espaçamento entre eles
+          */
+        }
+        <Stack spacing="4">
+          {
+            /*
+             o FormControl é um componente para ficar em volta da label e do input
+            */
+          }
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input 
+              name="email" 
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input 
+              name="password" 
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
