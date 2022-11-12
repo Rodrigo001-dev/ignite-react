fc59fe273e211722fa134fc10ad829fbdb38faec
Rodrigo <rodrigorael53@gmail.com>
2022-11-11 21:10:44 -0300

    separating the Pagination component into small components

---
+++ reactjs/aulas/04-dashgo/src/components/Pagination/PaginationItem.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@chakra-ui/react";
+
+interface PaginationItemProps {
+  pageNumber: number;
+  isCurrent?: boolean;
+};
+
+export function PaginationItem({
+  // pelo isCurrent ser opicional, é interessante eu definir um valor padrão/inicial
+  // para ele
+  isCurrent = false, 
+  pageNumber 
+}: PaginationItemProps) {
+  // se eu estou na página atual, vai apresentar um Button com essas propriedades
+  if (isCurrent) {
+    return (
+      <Button 
+        size="sm" 
+        fontSize="xs" 
+        width="4" 
+        colorScheme="pink" 
+        disabled
+        _disabled={{
+          bg: 'pink.500',
+          cursor: 'default',
+        }}
+      >
+        {pageNumber}
+      </Button>
+    );
+  };
+
+  // se não, eu apresento um Button com outras propriedades
+  return (
+    <Button 
+      size="sm" 
+      fontSize="xs" 
+      width="4" 
+      bg="gray.700"
+      _hover={{
+        bg: 'gray.500'
+      }}
+    >
+      {pageNumber}
+    </Button>
+  );
+};
---
+++ reactjs/aulas/04-dashgo/src/components/Pagination/index.tsx
@@ -0,0 +1,27 @@
+import { Box, Button, Stack } from "@chakra-ui/react";
+
+import { PaginationItem } from "./PaginationItem";
+
+export function Pagination() {
+  return (
+    <Stack
+      direction="row"
+      spacing="6"
+      mt="8"
+      justify="space-between"
+      align="center"
+    >
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <Stack direction="row" spacing="2">
+        <PaginationItem pageNumber={1} isCurrent />
+        <PaginationItem pageNumber={2} />
+        <PaginationItem pageNumber={3} />
+        <PaginationItem pageNumber={4} />
+        <PaginationItem pageNumber={5} />
+        <PaginationItem pageNumber={6} />
+      </Stack>
+    </Stack>
+  );
+};
