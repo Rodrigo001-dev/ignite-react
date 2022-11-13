b37c1d071a6426510a815d228ba72580f478609b
Rodrigo <rodrigorael53@gmail.com>
2022-11-12 23:17:41 -0300

    using react-hook-form to get input values with the concept of uncontrolled components

--- reactjs/aulas/04-dashgo/src/components/Form/Input.tsx
+++ reactjs/aulas/04-dashgo/src/components/Form/Input.tsx
@@ -4,33 +4,41 @@ import {
   Input as ChakraInput, 
   InputProps as ChakraInputProps 
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
 };
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    /*
-      o FormControl é um componente para ficar em volta da label e do input
-    */
-    <FormControl>
-      {/* caso o label exista vai apresentar em tela o FormLabel */}
-      { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> 
+  = ({ name, label, ...rest }, ref) => {
+    return (
+      /*
+        o FormControl é um componente para ficar em volta da label e do input
+      */
+      <FormControl>
+        {/* caso o label exista vai apresentar em tela o FormLabel */}
+        { !!label && <FormLabel htmlFor={name}>{label}</FormLabel> }
 
-      <ChakraInput 
-        name={name}
-        id={name}
-        focusBorderColor="pink.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: 'gray.900'
-        }}
-        size="lg"
-        {...rest}
-      />
-    </FormControl>
-  );
+        <ChakraInput 
+          name={name}
+          id={name}
+          focusBorderColor="pink.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          size="lg"
+          ref={ref}
+          {...rest}
+        />
+      </FormControl>
+    );
 };
+
+// o forwardRef vai fazer um encaminhamento da ref, ou seja, ele vai pegar a ref
+// que vai ser passada no SignIn e vai encaminhar para o componente chamado
+// InputBase
+export const Input = forwardRef(InputBase);
--- reactjs/aulas/04-dashgo/src/pages/index.tsx
+++ reactjs/aulas/04-dashgo/src/pages/index.tsx
@@ -1,8 +1,34 @@
 import { Button, Flex, Stack } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { Input } from '../components/Form/Input';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  // o react-hook-form utiliza do conceito de uncontrolled components que é uma
+  // forma mais performática de salvar as informações de inputs por exemplo
+  // porque da forma padrão com os controlled components que dessa forma
+  // controlamos o estado de inputs através de estados dentro do React, cada vez
+  // que o usuário digitar alguma coisa é salvo no estado
+  // utilizando o conceito de uncontrolled components é uma forma de conseguir
+  // acessar o valor do input só no momento em que for preciso, ou seja, não é
+  // armazenado o valor do input dentro de uma variável no estado, nós acessamos
+  // o valor do input no momento em que for necessário, isso é feito através de
+  // refs dentro do React
+  const { register, handleSubmit, formState } = useForm();
+
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -13,6 +39,10 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        // se eu passar somente a handleSignIn eu não vou conseguir ter acesso
+        // aos dados digitados dentro dos Inputs para isso eu tenho que passar
+        // a função handleSubmit e colocar ela por volta da função handleSignIn
+        onSubmit={handleSubmit(handleSignIn)}
       >
         {
           /*
@@ -21,11 +51,32 @@ export default function SignIn() {
           */
         }
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email" 
+            type="email" 
+            label="E-mail"
+            // utilizando o conceito de uncontrolled components eu preciso sempre
+            // passar a ref para o input mas por padrão o react-hook-form cria
+            // essas referências de forma automática por isso eu passo somente
+            // o register com o nome do campo
+            {...register('email')}
+          />
+          <Input
+            name="password" 
+            type="password" 
+            label="Senha"
+            {...register('password')}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex>
   )
