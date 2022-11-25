175d1a4769a4d3a4737d2d3d19146cbd57934a12
Rodrigo <rodrigorael53@gmail.com>
2022-11-25 16:17:14 -0300

    retrieving token from server-side

--- reactjs/aulas/05-nextauth/src/pages/index.tsx
+++ reactjs/aulas/05-nextauth/src/pages/index.tsx
@@ -1,4 +1,6 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
 
 import { AuthContext } from '../context/AuthContext';
 
@@ -38,4 +40,30 @@ export default function Home() {
       <button type="submit">Entrar</button>
     </form>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  // por eu estar pelo lado do servidor no Next, toda vez que eu for utilizar
+  // as funções do nookies, eu sempre vou passar como primeiro parâmetro o contexto(ctx)
+  // antes, quando eu estava utilizando pelo lado do browser eu passada undefined
+  const cookies = parseCookies(ctx);
+
+  // se dentro dos cookies existe o token
+  if (cookies['nextauth.token']) {
+    return {
+      // eu vou redirecionar o usuário para a página de dashboard
+      redirect: {
+        destination: '/dashboard',
+        // colocando o permanent como false eu falo que o redirecionamento não é
+        // permanente por conta do HTTP code para o browser entender se é um
+        // redirecionamento que sempre vai acontecer ou se só aconteceu dessa vez
+        // por conta de alguma condição
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
