2c835e275fd12e7980a38cbae3d3dd92ff819ac3
Rodrigo <rodrigorael53@gmail.com>
2022-12-25 18:52:21 -0300

    feat: using the memo to compare props

--- reactjs/aulas/06-performance-react/components/ProductItem.tsx
+++ reactjs/aulas/06-performance-react/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,39 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+// o memo vai evitar que seja criada uma nova versão do componente caso nenhuma
+// propriedade do componente tenha sido alterada
+// o memo faz uma shallow compare(comparação rasa) que é basicamente verificar
+// a igualdade das informações que eu tenho dentro das propriedades
+// mas no JavaScript quando comparamos um objeto com outro objeto({} === {})
+// sempre vai dar false, pois no caso de objetos e array o JavaScript faz uma
+// comparação de igualdade referencial que quando o JavaScript compara se os objetos
+// ou arrays estão ocupando a mesma posição na memória
+
+/*
+  Nós vamos utilizar o memo em 4 principais situações
+
+  1. Pure Functional Components: componentes apenas para abstrair alguma parte
+    visual da aplicação, as funções puras são basicamente funções que dados os
+    mesmos parâmetros sempre retornam o mesmo resultado
+
+  2. Renders too often: componentes que renderizam de mais
+  
+  3. Re-renders with same props: quando o componente renderiza novamente com as
+    as mesmas propriedades
+
+  4. Medium to big size: ganhamos muito mais performance quando o componente está
+    de um tamanho médio para um tamanho grande
+*/
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  // o Object.is faz uma comparação profunda
+  return Object.is(prevProps.product, nextProps.product)
+});
