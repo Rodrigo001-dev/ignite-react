8cf5cb87a164b3301a986bdc993d133761889590
Rodrigo <rodrigorael53@gmail.com>
2022-12-25 19:15:26 -0300

    feat: use the useMemo to memorize the totalPrice

--- reactjs/aulas/06-performance-react/components/SearchResults.tsx
+++ reactjs/aulas/06-performance-react/components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from 'react';
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -9,8 +11,26 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  // o useMemo vai memorizar entre as renderizações do componente o totalPrice
+  // para que ele não precise ser recalculado toda vez do zero
+  // e o useMemo também server para evitar que uma variável ocupe um novo local
+  // na memória quando estamos utilizando essa variável para ser repassada para
+  // um componente filho
+  /*
+    Vamos utilizar o useMemo em 2 situações:
+    1. Cálculos pesados
+    2. Igualdade referencial (quando repassa aquela informação a um componente filho)
+  */
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0)
+  }, [results]);
+  
   return (
     <div>
+      <h2>{totalPrice}</h2>
+
       {results.map(product => {
         return (
           <ProductItem product={product} />
