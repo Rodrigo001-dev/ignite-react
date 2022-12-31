54a319ee647c52ff56f7d91a13f688072b1a9101
Rodrigo <rodrigorael53@gmail.com>
2022-12-31 14:55:51 -0300

    fix: change the memo

--- reactjs/desafios/ignite-challenge-08/src/components/SideBar.tsx
+++ reactjs/desafios/ignite-challenge-08/src/components/SideBar.tsx
@@ -37,6 +37,4 @@ function SideBarComponent({
   );
 }
 
-export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps.genres, nextProps.genres);
-});
+export const SideBar = memo(SideBarComponent);
