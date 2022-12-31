17cffa2d9deb9e606687bbd57f61c4ef08930bff
Rodrigo <rodrigorael53@gmail.com>
2022-12-31 01:33:36 -0300

    feat: use the memo to verify the genres in SideBar

--- reactjs/desafios/ignite-challenge-08/src/components/SideBar.tsx
+++ reactjs/desafios/ignite-challenge-08/src/components/SideBar.tsx
@@ -1,26 +1,29 @@
+import { memo } from "react";
 import { Button } from "./Button";
 
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
   buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({
+function SideBarComponent({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  buttonClickCallback,
 }: SideBarProps) {
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
+        {genres.map((genre) => (
           <Button
             key={String(genre.id)}
             title={genre.title}
@@ -30,7 +33,10 @@ export function SideBar({
           />
         ))}
       </div>
-
     </nav>
-  )
+  );
 }
+
+export const SideBar = memo(SideBarComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.genres, nextProps.genres);
+});
