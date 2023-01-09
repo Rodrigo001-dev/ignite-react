d71975c458f58ac3b9be90fb405bac3aa7eddd2d
Rodrigo <rodrigorael53@gmail.com>
2023-01-09 02:27:27 -0300

    test: create tests to Home page

---
+++ reactjs/aulas/03-ignews/src/tests/pages/Home.spec.tsx
@@ -0,0 +1,43 @@
+import { render, screen } from "@testing-library/react";
+import Home, { getStaticProps } from "../../pages";
+import { stripe } from "../../services/stripe";
+
+jest.mock("next/router");
+jest.mock("next-auth/client", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+jest.mock("../../services/stripe");
+
+describe("Home page", () => {
+  it("renders correctly", () => {
+    render(<Home product={{ priceId: "fake-price-id", amount: "R$10,00" }} />);
+
+    expect(screen.getByText("for R$10,00 month")).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      id: "fake-price-id",
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    console.log({ response });
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "fake-price-id",
+            amount: "$10.00",
+          },
+        },
+      })
+    );
+  });
+});
