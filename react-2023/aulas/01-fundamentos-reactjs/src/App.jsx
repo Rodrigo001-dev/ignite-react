2b54c70c17c56df8493a7ef96fd28dd3bfe2ee3e
Rodrigo <rodrigorael53@gmail.com>
2023-04-19 13:05:49 -0300

    :tada: initial commit

---
+++ react-2023/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <h1>Hello World</h1>
+    </div>
+  );
+}
