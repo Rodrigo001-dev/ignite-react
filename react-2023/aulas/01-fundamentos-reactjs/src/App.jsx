fd65ae31213b8cb451b2db1a2787ed7e903d2b48
Rodrigo <rodrigorael53@gmail.com>
2023-04-20 10:45:54 -0300

    :sparkles: feat: Post component

--- react-2023/aulas/01-fundamentos-reactjs/src/App.jsx
+++ react-2023/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 import "./global.css";
@@ -11,7 +12,9 @@ export function App() {
 
       <div className={styles.wrapper}>
         <Sidebar />
-        <main></main>
+        <main>
+          <Post />
+        </main>
       </div>
     </div>
   );
---
+++ react-2023/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,39 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/Rodrigo001-dev.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Rodrigo Rael</strong>
+            <span>FullStack Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Abril ás 08:13h" datatype="2023-04-11 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href=""> jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
