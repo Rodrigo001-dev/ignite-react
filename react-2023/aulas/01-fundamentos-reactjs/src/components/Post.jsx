e313d1af92ab5da61ee89d3cc1f9d3bbe7696684
Rodrigo <rodrigorael53@gmail.com>
2023-04-20 12:11:18 -0300

    :recycle: refactor: Avatar component

--- react-2023/aulas/01-fundamentos-reactjs/src/components/Post.jsx
+++ react-2023/aulas/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 
 import styles from "./Post.module.css";
@@ -7,10 +8,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/Rodrigo001-dev.png"
-          />
+          <Avatar src="https://github.com/Rodrigo001-dev.png" />
           <div className={styles.authorInfo}>
             <strong>Rodrigo Rael</strong>
             <span>FullStack Developer</span>
--- react-2023/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ react-2023/aulas/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from "phosphor-react";
 
 import styles from "./Sidebar.module.css";
+import { Avatar } from "./Avatar";
 
 export function Sidebar() {
   return (
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/Rodrigo001-dev.png"
-        />
+        <Avatar src="https://github.com/Rodrigo001-dev.png" />
 
         <strong>Rodrigo Rael</strong>
         <span>FullStack Developer</span>
