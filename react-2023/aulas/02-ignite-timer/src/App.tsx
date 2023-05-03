934b6f853c54041b97064c1355a8e500b6d9a293
Rodrigo <rodrigorael53@gmail.com>
2023-05-03 10:58:12 -0300

    :lipstick: feat: global styles

--- react-2023/aulas/02-ignite-timer/src/App.tsx
+++ react-2023/aulas/02-ignite-timer/src/App.tsx
@@ -1,3 +1,13 @@
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+
 export function App() {
-  return <h1>Hellow World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
