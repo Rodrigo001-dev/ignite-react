65723517142e0b77c4bb4025753cbc9af00fcad8
Rodrigo <rodrigorael53@gmail.com>
2023-05-17 09:50:53 -0300

    :sparkles: feat: cart context

--- react-2023/desafios/ignite-challenge-02/src/App.tsx
+++ react-2023/desafios/ignite-challenge-02/src/App.tsx
@@ -3,15 +3,19 @@ import { ThemeProvider } from "styled-components";
 
 import { Router } from "./Router";
 
+import { CartContextProvider } from "./contexts/CartContext";
+
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyles } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <CartContextProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </CartContextProvider>
       <GlobalStyles />
     </ThemeProvider>
   );
