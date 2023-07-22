bdddb2c349cce9895440e47b760eda40aaa85ce4
Rodrigo <rodrigorael53@gmail.com>
2023-07-22 12:05:52 -0300

    :sparkles: feat: Header component

--- react-2023/aulas/04-ignite-shop/src/pages/_app.tsx
+++ react-2023/aulas/04-ignite-shop/src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import { Roboto } from 'next/font/google'
 
 import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
+
+import logoImg from '../assets/logo.svg'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -12,8 +16,12 @@ globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
       <Component {...pageProps} />
-    </main>
+    </Container>
   )
 }
