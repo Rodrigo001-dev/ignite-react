f38dbed36174b82eb015fbc540a289a1583f299e
Rodrigo <rodrigorael53@gmail.com>
2023-07-22 16:55:07 -0300

    :sparkles: feat: fetch checkout data in success

--- react-2023/aulas/04-ignite-shop/src/pages/success.tsx
+++ react-2023/aulas/04-ignite-shop/src/pages/success.tsx
@@ -1,20 +1,56 @@
 import Link from 'next/link'
+import { GetServerSideProps } from 'next'
+import Stripe from 'stripe'
+
+import { stripe } from '@/lib/stripe'
 
 import { SuccessContainer, ImageContainer } from '@/styles/pages/success'
+import Image from 'next/image'
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Rodrigo Rael</strong>, sua camiseta{' '}
-        <strong>Beyond the Limits</strong> já está a caminho da sua casa
+        Uhuul <strong>{customerName}</strong>, sua camiseta{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa
       </p>
 
       <Link href={'/'}>Voltar ao catálogo</Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const customerName = session.customer_details!.name
+  const product = session.line_items!.data[0].price?.product as Stripe.Product
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imgeUrl: product.images[0],
+      },
+    },
+  }
 }
