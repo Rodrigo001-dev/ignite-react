f66663dcc410f0c1aeb1a12bc7eeb33ca23001ba
Rodrigo <rodrigorael53@gmail.com>
2023-07-26 16:10:45 -0300

    :sparkles: feat: Avatar component

---
+++ react-2023/aulas/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args:{
+    src: 'https://github.com/Rodrigo001-dev.png',
+    alt: 'Rodrigo Rael'
+  }
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args:{
+    src: undefined
+  }
+}
