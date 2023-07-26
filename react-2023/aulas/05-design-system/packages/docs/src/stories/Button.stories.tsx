0dd045da9737e98e758ad9618c4795b36477d9af
Rodrigo <rodrigorael53@gmail.com>
2023-07-26 16:52:53 -0300

    :sparkles: feat: Button component

---
+++ react-2023/aulas/05-design-system/packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,52 @@
+import { ArrowRight } from 'phosphor-react'
+
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send',
+  }
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new'
+  }
+}
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel',
+  }
+}
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Próximo passo
+        <ArrowRight weight="bold" />
+      </>
+    )
+  }
+}
+
+export const Disable: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+  }
+}
+
